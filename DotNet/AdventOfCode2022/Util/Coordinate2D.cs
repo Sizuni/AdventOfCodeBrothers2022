847a81c6b128cce707c98a8b02b03a742c0705e0
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-11-30 19:51:48 +0100

    Add Util project

---
+++ DotNet/AdventOfCode2022/Util/Coordinate2D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Util
+{
+    public class Coordinate2D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinate2D()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        /// <summary>
+        /// Create and initialize a coordinate by parsing a string value containing 2 numbers separated by a comma
+        /// </summary>
+        /// <param name="value">Coordinate with format of "0,0"</param>
+        public Coordinate2D(string value)
+        {
+            string[] splitValues = value.Split(',');
+            X = Convert.ToInt32(splitValues[0]);
+            Y = Convert.ToInt32(splitValues[1]);
+        }
+    }
+}
---
+++ DotNet/AdventOfCode2022/Util/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Util
+{
+    public class FileReader
+    {
+        public static List<string> ReadAllLinesFromInputFile()
+        {
+            return ReadAllLinesFrom("input.txt");
+        }
+
+        public static List<string> ReadAllLinesFromTestInputFile()
+        {
+            return ReadAllLinesFrom("testInput.txt");
+        }
+
+        public static List<string> ReadAllLinesFrom(string file)
+        {
+            string currentFolder = Directory.GetCurrentDirectory();
+            string inputFile = Path.Combine(currentFolder, file);
+            return new List<string>(File.ReadAllLines(inputFile));
+        }
+    }
+}
