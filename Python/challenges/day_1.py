19e2b80ef6865a73d3cc1b73e2d7e53b0e8a3111
LucasToirkensPXL <lucas.toirkens@googlemail.com>
2022-12-01 20:02:17 +0100

    Reuse code

--- Python/challenges/day_1.py
+++ Python/challenges/day_1.py
@@ -1,17 +1,6 @@
-def a(input):
-    highest = 0
-    sum = 0
-    for calories in input:
-        if calories == '':
-            if sum > highest:
-                highest = sum
-            sum = 0
-        else:
-            sum += int(calories)
-    return highest
-
-def b(input):
-    highest = [0,0,0]
+def get_highest_sum(emptyList: list, data):
+    input = data
+    highest = emptyList
     sum = 0
     for calories in input:
         if calories == '':
@@ -21,4 +10,12 @@ def b(input):
             sum = 0
         else:
             sum += int(calories)
+    return highest
+
+def a(input):
+    highest = get_highest_sum([0], input)
+    return highest[0]
+
+def b(input):
+    highest = get_highest_sum([0,0,0], input)
     return highest[0] + highest[1] + highest[2]
