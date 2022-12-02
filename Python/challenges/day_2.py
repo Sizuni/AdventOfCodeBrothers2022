32bae8a035994b8819b0f6b3f6de2f2d59ff921b
LucasToirkensPXL <lucas.toirkens@googlemail.com>
2022-12-02 20:59:40 +0100

    Solve day 2 part 2

--- Python/challenges/day_2.py
+++ Python/challenges/day_2.py
@@ -1,9 +1,14 @@
-def get_result(round):
+def get_result(round, strategy=False):
     score_matrix = [3, 6, 0]
     moves = round.split(" ")
     opponent = int(ord(moves[0]) - 65)
     you = int(ord(moves[1]) - 88)
-    return (score_matrix[3 - opponent:] + score_matrix[:3 - opponent])[you] + you + 1
+    if not strategy:
+        return (score_matrix[3 - opponent:] + score_matrix[:3 - opponent])[you] + you + 1
+    else:
+        moves = [0, 1, 2, 0, 1, 2]
+        you = moves[opponent - 1 + you]
+        return (score_matrix[3 - opponent:] + score_matrix[:3 - opponent])[you] + you + 1
 
 def a(input):
     points = 0
@@ -12,4 +17,7 @@ def a(input):
     return points
 
 def b(input):
-    pass
+    points = 0
+    for round in input:
+        points += get_result(round, True)
+    return points
