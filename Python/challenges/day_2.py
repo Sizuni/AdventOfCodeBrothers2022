afc479d21e94edd84e939a02c849f072879b0364
LucasToirkensPXL <lucas.toirkens@gmail.com>
2022-12-02 08:01:47 +0100

    Current version

--- Python/challenges/day_2.py
+++ Python/challenges/day_2.py
@@ -1,5 +1,37 @@
+def get_result(round):
+    moves = round.split(" ")
+    opponent = moves[0]
+    you = moves[1]
+    print(moves)
+    if opponent == "A":
+        if you == "X":
+            return 3 + 1
+        elif you == "Y":
+            return 6 + 2
+        else:
+            return 0 + 3
+    elif opponent == "B":
+        if you == "X":
+            return 0 + 1
+        elif you == "Y":
+            return 3 + 2
+        else:
+            return 6 + 3
+    else:
+        if you == "X":
+            return 6 + 1
+        elif you == "Y":
+            return 3 + 2
+        else:
+            return 0 + 3
+
+
 def a(input):
-    return input
+    points = 0
+    for round in input:
+        points += get_result(round)
+        print(points)
+    return points
 
 def b(input):
     pass
