15a41e0a6d23899f68d29042f463ebbd4879089c
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-03 17:51:03 +0100

    Refactor common behavior to ItemService

--- DotNet/AdventOfCode2022/Day03RucksackReorganization/Domain/Group.cs
+++ DotNet/AdventOfCode2022/Day03RucksackReorganization/Domain/Group.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Day03RucksackReorganization.Services;
 
 namespace Day03RucksackReorganization.Domain
 {
@@ -19,61 +17,7 @@ namespace Day03RucksackReorganization.Domain
 
         public char FindCommonItemInAllRucksacks()
         {
-            int[] itemCountFirstRucksack = new int[52];
-            foreach (char item in FirstRucksack.Content)
-            {
-                if (char.IsLower(item))
-                {
-                    itemCountFirstRucksack[item - 'a']++;
-                }
-                else
-                {
-                    itemCountFirstRucksack[item - 'A' + 26]++;
-                }
-            }
-
-            int[] itemCountSecondRucksack = new int[52];
-            foreach (char item in SecondRucksack.Content)
-            {
-                if (char.IsLower(item))
-                {
-                    itemCountSecondRucksack[item - 'a']++;
-                }
-                else
-                {
-                    itemCountSecondRucksack[item - 'A' + 26]++;
-                }
-            }
-
-            int[] itemCountThirdRucksack = new int[52];
-            foreach (char item in ThirdRucksack.Content)
-            {
-                if (char.IsLower(item))
-                {
-                    itemCountThirdRucksack[item - 'a']++;
-                }
-                else
-                {
-                    itemCountThirdRucksack[item - 'A' + 26]++;
-                }
-            }
-
-            bool[] itemContentFirstRucksack = Array.ConvertAll(itemCountFirstRucksack, count => count > 0);
-            bool[] itemContentSecondRucksack = Array.ConvertAll(itemCountSecondRucksack, count => count > 0);
-            bool[] itemContentThirdRucksack = Array.ConvertAll(itemCountThirdRucksack, count => count > 0);
-
-            for (int i = 0; i < 52; i++)
-            {
-                if (itemContentFirstRucksack[i] && itemContentSecondRucksack[i] && itemContentThirdRucksack[i])
-                {
-                    if (i < 26)
-                    {
-                        return Convert.ToChar(i + 'a');
-                    }
-                    return Convert.ToChar(i - 26 + 'A');
-                }
-            }
-            return '0';
+            return ItemService.FindCommonItem(FirstRucksack.Content, SecondRucksack.Content, ThirdRucksack.Content);
         }
     }
 }
