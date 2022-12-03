e03e394d20eec59f02aa7d304efbb9b3bb5addc1
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-03 16:29:03 +0100

    Add day 3 part 1

---
+++ DotNet/AdventOfCode2022/Day03RucksackReorganization/Domain/Rucksack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Day03RucksackReorganization.Domain
+{
+    public class Rucksack
+    {
+        public string FirstCompartment { get; private set; }
+        public string SecondCompartment { get; private set; }
+
+        public Rucksack(string content)
+        {
+            int amount = content.Length;
+            FirstCompartment = content.Substring(0, amount / 2);
+            SecondCompartment = content.Substring(amount / 2);
+        }
+
+        public char FindCommonItemInBothCompartments()
+        {
+            int[] itemCountFirstCompartment = new int[52];
+            foreach (char item in FirstCompartment)
+            {
+                if (char.IsLower(item))
+                {
+                    itemCountFirstCompartment[item - 'a']++;
+                }
+                else
+                {
+                    itemCountFirstCompartment[item - 'A' + 26]++;
+                }
+            }
+
+            int[] itemCountSecondCompartment = new int[52];
+            foreach (char item in SecondCompartment)
+            {
+                if (char.IsLower(item))
+                {
+                    itemCountSecondCompartment[item - 'a']++;
+                }
+                else
+                {
+                    itemCountSecondCompartment[item - 'A' + 26]++;
+                }
+            }
+
+            bool[] itemContentFirstCompartment = Array.ConvertAll(itemCountFirstCompartment, count => count > 0);
+            bool[] itemContentSecondCompartment = Array.ConvertAll(itemCountSecondCompartment, count => count > 0);
+
+            for (int i = 0; i < 52; i++)
+            {
+                if (itemContentFirstCompartment[i] && itemContentSecondCompartment[i])
+                {
+                    if (i < 26)
+                    {
+                        return Convert.ToChar(i + 'a');
+                    }
+                    return Convert.ToChar(i - 26 + 'A');
+                }
+            }
+            return '0';
+        }
+
+        public static int GetPriorityForItem(char item)
+        {
+            if (char.IsLower(item))
+            {
+                return item - 'a' + 1;
+            }
+            return item - 'A' + 27;
+        }
+    }
+}
---
+++ DotNet/AdventOfCode2022/Day03RucksackReorganization/Program.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using Day03RucksackReorganization.Domain;
+using System.Collections.Generic;
+using System.Linq;
+using Util;
+
+namespace Day03RucksackReorganization
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var summary = BenchmarkRunner.Run<ProgramBenchmarker>();
+            SolutionWriter<int>.WriteSolution(PartOne(), PartTwo());
+        }
+
+        public static int PartOne()
+        {
+            List<string> input = FileReader.ReadAllLinesFromInputFile();
+
+            return input.Select(line => new Rucksack(line))
+                .Select(r => r.FindCommonItemInBothCompartments())
+                .Select(Rucksack.GetPriorityForItem)
+                .Sum();
+        }
+
+        public static int PartTwo()
+        {
+            return 0;
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class ProgramBenchmarker
+    {
+        [Benchmark]
+        public void PartOne()
+        {
+            Program.PartOne();
+        }
+
+        [Benchmark]
+        public void PartTwo()
+        {
+            Program.PartTwo();
+        }
+    }
+}
