f64582b1cd3b73839e02832433063c07ffc483fc
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-04 16:36:01 +0100

    Add day 4 part 2

--- DotNet/AdventOfCode2022/Day04CampCleanup/Domain/SectionAssignmentPair.cs
+++ DotNet/AdventOfCode2022/Day04CampCleanup/Domain/SectionAssignmentPair.cs
@@ -24,5 +24,17 @@
             return (ElfOneFirstSection >= ElfTwoFirstSection && ElfOneLastSection <= ElfTwoLastSection) 
                 || (ElfTwoFirstSection >= ElfOneFirstSection && ElfTwoLastSection <= ElfOneLastSection);
         }
+
+        /// <summary>
+        /// Checks whether at least one section of one elf overlaps with the other elf.
+        /// </summary>
+        /// <returns>True when at least one section of one elf is overlapped by the other elf, false otherwise.</returns>
+        public bool HasOverlap()
+        {
+            return (ElfOneFirstSection >= ElfTwoFirstSection && ElfOneFirstSection <= ElfTwoLastSection)
+                || (ElfOneLastSection >= ElfTwoFirstSection && ElfOneLastSection <= ElfTwoLastSection)
+                || (ElfTwoFirstSection >= ElfOneFirstSection && ElfTwoFirstSection <= ElfOneLastSection)
+                || (ElfTwoLastSection >= ElfOneFirstSection && ElfTwoLastSection <= ElfOneLastSection);
+        }
     }
 }
