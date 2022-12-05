867a596c5be14e475b6abc51e0899db6ad31aff8
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-05 17:27:37 +0100

    Add day 5 part 1

---
+++ DotNet/AdventOfCode2022/Day05SupplyStacks/Domain/ProcedureInstruction.cs
@@ -0,0 +1,16 @@
+﻿namespace Day05SupplyStacks.Domain
+{
+    public class ProcedureInstruction
+    {
+        public int Amount { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+
+        public ProcedureInstruction(int amount, int from, int to)
+        {
+            Amount = amount;
+            From = from;
+            To = to;
+        }
+    }
+}
