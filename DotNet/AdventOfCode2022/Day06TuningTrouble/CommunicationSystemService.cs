ed462c5e2d592e386b1b2f61b0424641a8535360
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-08 17:14:31 +0100

    Add day 6 part 1

---
+++ DotNet/AdventOfCode2022/Day06TuningTrouble/CommunicationSystemService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day06TuningTrouble
+{
+    public class CommunicationSystemService
+    {
+        public static int FindStartOfPacketMarker(string datastream, int uniqueCharacterSequenceLength = 4)
+        {
+            // Prepare queue with the required sequence length
+            Queue<char> sequence = new Queue<char>(datastream.Substring(0, uniqueCharacterSequenceLength));
+            if (sequence.Distinct().Count() == uniqueCharacterSequenceLength)
+            {
+                return uniqueCharacterSequenceLength;
+            }
+
+            for (int i = uniqueCharacterSequenceLength; i < datastream.Length; i++)
+            {
+                sequence.Dequeue();
+                sequence.Enqueue(datastream.ElementAt(i));
+                if (sequence.Distinct().Count() == uniqueCharacterSequenceLength)
+                {
+                    return i + 1;
+                }
+                
+            }
+
+            return 0;
+        }
+    }
+}
