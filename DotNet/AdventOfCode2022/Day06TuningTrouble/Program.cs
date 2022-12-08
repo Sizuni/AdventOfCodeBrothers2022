d42a89c8ceb3921e9e4e30d7eb223144a9dd0dba
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-08 17:14:31 +0100

    Add day 6 part 1

---
+++ DotNet/AdventOfCode2022/Day06TuningTrouble/Program.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using Util;
+
+namespace Day06TuningTrouble
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var summary = BenchmarkRunner.Run<ProgramBenchmarker>();
+            SolutionWriter<int>.WriteSolution(PartOne(), PartTwo());
+        }
+
+        public static int PartOne()
+        {
+            string input = FileReader.ReadAllLinesFromInputFile()[0];
+
+            return CommunicationSystemService.FindStartOfPacketMarker(input, 4);
+        }
+
+        public static int PartTwo()
+        {
+            string input = FileReader.ReadAllLinesFromInputFile()[0];
+            return 0;
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class ProgramBenchmarker
+    {
+        [Benchmark]
+        public void PartOne()
+        {
+            Program.PartOne();
+        }
+
+        [Benchmark]
+        public void PartTwo()
+        {
+            Program.PartTwo();
+        }
+    }
+}
