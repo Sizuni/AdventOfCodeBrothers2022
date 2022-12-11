75238a3e75c413851566e4c1a2634e67307f5389
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-11 17:33:18 +0100

    Add day 9 part 1

---
+++ DotNet/AdventOfCode2022/Day09RopeBridge/Services/RopePositionMapperService.cs
@@ -0,0 +1,57 @@
+﻿using Day09RopeBridge.Domain;
+using System.Collections.Generic;
+using Util;
+
+namespace Day09RopeBridge.Services
+{
+    public class RopePositionMapperService
+    {
+        private Rope rope;
+        private Dictionary<Coordinate2D, int> tailPositionCounter;
+
+        public RopePositionMapperService()
+        {
+            rope = new Rope();
+            tailPositionCounter = new Dictionary<Coordinate2D, int>();
+
+            Coordinate2D tailPosition = new Coordinate2D { X = rope.TailPosition.X, Y = rope.TailPosition.Y };
+            tailPositionCounter.Add(tailPosition, 1);
+        }
+
+        public void MoveRight()
+        {
+            rope.MoveRight();
+            IncrementTailPositionCounter();
+        }
+
+        public void MoveLeft()
+        {
+            rope.MoveLeft();
+            IncrementTailPositionCounter();
+        }
+
+        public void MoveUp()
+        {
+            rope.MoveUp();
+            IncrementTailPositionCounter();
+        }
+
+        public void MoveDown()
+        {
+            rope.MoveDown();
+            IncrementTailPositionCounter();
+        }
+
+        public int GetAmountOfPositionsVisited()
+        {
+            return tailPositionCounter.Values.Count;
+        }
+
+        private void IncrementTailPositionCounter()
+        {
+            Coordinate2D tailPosition = new Coordinate2D { X = rope.TailPosition.X, Y = rope.TailPosition.Y };
+            tailPositionCounter.TryGetValue(tailPosition, out int count);
+            tailPositionCounter[tailPosition] = count + 1;
+        }
+    }
+}
