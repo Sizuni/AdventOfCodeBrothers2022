478d6593284eb064d541e6a82531be4a14488ed0
Dylan Toirkens <dylan.toirkens@gmail.com>
2022-12-11 18:21:10 +0100

    Refactor day 9, add parse method

--- DotNet/AdventOfCode2022/Day09RopeBridge/Program.cs
+++ DotNet/AdventOfCode2022/Day09RopeBridge/Program.cs
@@ -21,36 +21,7 @@ namespace Day09RopeBridge
             RopePositionMapperService service = new RopePositionMapperService(new ShortRope());
             foreach (string line in input)
             {
-                string[] lineInfo = line.Split(' ');
-                string direction = lineInfo[0];
-                int amount = Convert.ToInt32(lineInfo[1]);
-                switch(direction)
-                {
-                    case "R":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveRight();
-                        }
-                        break;
-                    case "L":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveLeft();
-                        }
-                        break;
-                    case "U":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveUp();
-                        }
-                        break;
-                    case "D":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveDown();
-                        }
-                        break;
-                }
+                ParseLine(line, service);
             }
 
             return service.GetAmountOfPositionsVisited();
@@ -62,39 +33,44 @@ namespace Day09RopeBridge
             RopePositionMapperService service = new RopePositionMapperService(new LongRope(10));
             foreach (string line in input)
             {
-                string[] lineInfo = line.Split(' ');
-                string direction = lineInfo[0];
-                int amount = Convert.ToInt32(lineInfo[1]);
-                switch (direction)
-                {
-                    case "R":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveRight();
-                        }
-                        break;
-                    case "L":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveLeft();
-                        }
-                        break;
-                    case "U":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveUp();
-                        }
-                        break;
-                    case "D":
-                        for (int i = 0; i < amount; i++)
-                        {
-                            service.MoveDown();
-                        }
-                        break;
-                }
+                ParseLine(line, service);
             }
 
             return service.GetAmountOfPositionsVisited();
+        }
+
+        private static void ParseLine(string line, RopePositionMapperService service)
+        {
+            string[] lineInfo = line.Split(' ');
+            string direction = lineInfo[0];
+            int amount = Convert.ToInt32(lineInfo[1]);
+            switch (direction)
+            {
+                case "R":
+                    for (int i = 0; i < amount; i++)
+                    {
+                        service.MoveRight();
+                    }
+                    break;
+                case "L":
+                    for (int i = 0; i < amount; i++)
+                    {
+                        service.MoveLeft();
+                    }
+                    break;
+                case "U":
+                    for (int i = 0; i < amount; i++)
+                    {
+                        service.MoveUp();
+                    }
+                    break;
+                case "D":
+                    for (int i = 0; i < amount; i++)
+                    {
+                        service.MoveDown();
+                    }
+                    break;
+            }
         }
     }
 
